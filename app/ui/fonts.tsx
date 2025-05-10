e9710c6ec89eb7d8908564851e4107ff8d24f6db
mdsaddamhossain1 <mdsaddam.hossain@navanarealestate.com>
2025-05-10 16:55:31 +0600

    Roboto font add

--- app/ui/fonts.tsx
+++ app/ui/fonts.tsx
@@ -1,3 +1,9 @@
-import { Inter } from 'next/font/google';
+import { Inter, Roboto } from 'next/font/google';
 
 export const inter = Inter({ subsets: ['latin'] });
+export const roboto = Roboto({
+  weight: ['400', '700'], // Specify the font weights you need
+  subsets: ['latin'], // Specify character subsets
+  display: 'swap', // Optional: improves font loading behavior
+  variable: '--font-roboto', // Optional: creates a CSS variable
+});
